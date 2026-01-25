44932992bc6e673591e3a4ebfd9aa4414e7cad0f
Lawarna Aree <supremaree@gmail.com>
2026-01-25 17:22:33 +0545

    feat(sky): add celestial objects and refine background layout

--- src/components/Sky/Sky.jsx
+++ src/components/Sky/Sky.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import cloud from '../../assets/images/cloud.png'
 import earth from '../../assets/images/earth.png'
+import comet from '../../assets/images/comet.png'
+import astronaut from '../../assets/images/astronaut2.png'
+import astronaut2 from '../../assets/images/astronaut.png'
 import './Sky.css'
 
 const Sky = () => {
@@ -21,10 +24,13 @@ const Sky = () => {
     }
 
     return (
-        <div>
+        <div className="main-div">
             <div className="parent-clouds">{smallClouds}</div>
             <div className="parent-bigclouds">{bigClouds}</div>
             <div className="parent-earth"><img src={earth} /></div>
+            <div className="parent-comet"><img src={comet} /></div>
+            <div className="parent-astronaut"><img src={astronaut} /></div>
+            <div className="parent-astronaut2"><img src={astronaut2} /></div>
         </div >
     )
 
